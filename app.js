01e1d4f220c81dd9f2aa20b3d8f8fa4207ac9028
Alexander Kuznetsov <alexander.kuznetsov96@gmail.com>
2017-02-04 14:42:11 -0500

    get message text

--- app.js
+++ app.js
@@ -124,8 +124,54 @@ const actions = {
             return Promise.resolve()
         }
     },
-    // You should implement your custom actions here
-    // See https://wit.ai/docs/quickstart
+    getRecipe({context, entities}) {
+        return new Promise(function (resolve, reject) {
+            var mealTime = firstEntityValue(entities, "mealTime")
+            var meal = firstEntityValue(entities, "meal")
+            var flavor = firstEntityValue(entities, "flavor")
+            var ingredients = firstEntityValue(entities, "ingredients")
+            var mealType = firstEntityValue(entities, "mealType")
+            var api_ID = 'e041f1a3';
+            var app_key = '2598fe134189c3ff6692c77e1e80a4c3';
+            var content;
+            /*
+            request('https://api.edamam.com/search?q=chicken&app_id=' + api_ID + '&app_key=' + app_key, function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                content = body;
+              }
+            })
+            */
+            context.recipe = ''
+            if (meal) {
+                context.recipe = context.recipe + meal;
+            }
+            else {
+                context.recipe = 'Something';
+            }
+            if (mealTime) {
+                context.recipe = context.recipe + ' for ' + mealTime
+            }
+            if (flavor) {
+                var flavor = '';
+                for (var u of entities.flavor) {
+                    console.log(u.value);
+                    flavor = flavor + u.value + ',';
+                }
+                context.recipe = context.recipe + ' that is ' + flavor;
+            }
+            if (ingredients) {
+                var ingredients = '';
+                for (var u of entities.ingredients) {
+                    console.log(u.value);
+                    ingredients = ingredients + u.value + ',';
+                }
+                context.recipe = context.recipe + ' using ' + ingredients;
+            }
+            delete context.missingMealTime;
+
+            return resolve(context);
+        });
+    }
 };
 
 // Setting up our bot
@@ -244,6 +290,7 @@ app.post('/webhook', (req, res) => {
 
                     // We retrieve the message content
                     const attachments = event.message.attachments;
+                    const text = event.message.text;
 
                     if (attachments) {
                         // We received an attachment
