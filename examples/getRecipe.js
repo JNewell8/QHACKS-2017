e8a99f1a57fd2dbc303ef3e3727f085650fe4307
James Newell <jamesrnewell8@gmail.com>
2017-02-04 14:36:37 -0500

    Updated architecture

--- examples/getRecipe.js
+++ examples/getRecipe.js
@@ -7,6 +7,7 @@ var http = require("http");
 var https = require("https");
 var request = require("request");
 
+
 try {
   // if running from repo
   Wit = require('../').Wit;
@@ -51,26 +52,53 @@ const actions = {
   getRecipe({context, entities}) {
   return new Promise(function(resolve, reject) {
     var mealTime = firstEntityValue(entities, "mealTime")
-    if (mealTime) {
-        var api_ID = 'e041f1a3';
-        var app_key = '2598fe134189c3ff6692c77e1e80a4c3';
-        var content;
-        request('https://api.edamam.com/search?q=chicken&app_id=' + api_ID + '&app_key=' + app_key, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            content = body;
-          }
-        })
-        body = JSON.parse(content);
-        context.recipe = body.hits[1].recipe.uri;
-        delete context.missingLocation;
-    } else {
-      context.missingLocation = true;
-      delete context.recipe;
+    var meal = firstEntityValue(entities, "meal")
+    var flavor = firstEntityValue(entities, "flavor")
+    var ingredients = firstEntityValue(entities, "ingredients")
+    var mealType = firstEntityValue(entities, "mealType")
+    var api_ID = 'e041f1a3';
+    var app_key = '2598fe134189c3ff6692c77e1e80a4c3';
+    var content;
+    /*
+    request('https://api.edamam.com/search?q=chicken&app_id=' + api_ID + '&app_key=' + app_key, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        content = body;
+      }
+    })
+    */
+    context.recipe = ''
+    if(meal) {
+        context.recipe = context.recipe + meal;
     }
+    else{
+        context.recipe = 'Something';
+    }
+    if(mealTime){
+        context.recipe = context.recipe + ' for ' + mealTime
+    }
+    if (flavor){
+        var flavor = '';
+        for (var u of entities.flavor){
+            console.log(u.value);
+            flavor = flavor + u.value + ',';
+        }
+      context.recipe = context.recipe + ' that is ' + flavor;  
+    }
+    if (ingredients){
+        var ingredients = '';
+        for (var u of entities.ingredients){
+            console.log(u.value);
+            ingredients = ingredients + u.value + ',';
+        }
+        context.recipe = context.recipe + ' using ' + ingredients;
+    }
+    delete context.missingMealTime;
+    
     return resolve(context);
   });
 }
 ,
+/*
 useIngredients({context, entities}) {
   return new Promise(function(resolve, reject) {
     var meal = firstEntityValue(entities, "meal")
@@ -84,6 +112,7 @@ useIngredients({context, entities}) {
     return resolve(context);
   });
 },
+*/
 };
 
 const client = new Wit({accessToken, actions});
