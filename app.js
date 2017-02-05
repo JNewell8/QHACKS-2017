e0289e30a5606752c8ea69385525f844fdabb179
Alexander Kuznetsov <alexander.kuznetsov96@gmail.com>
2017-02-05 02:01:53 -0500

    added calorie information

--- app.js
+++ app.js
@@ -1133,7 +1133,7 @@ function QueryRecipeApi(type, meal, flavour, ingredients, cuisine, diet, Instruc
                 for (var i = 0; i < hits.length; i++) {
                     var hit = hits[i];
                     context.recipe += hit.recipe.label + "\n";
-                    context.recipe += hit.recipe.summary + "\n";
+                    context.recipe += "Calories: " + hit.recipe.calories + "\n";
                     context.recipe += "Servings: " + hit.recipe.yield + "\n";
                     context.recipe += hit.recipe.url + "\n\n";
                 }
